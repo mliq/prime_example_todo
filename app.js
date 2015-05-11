4df6dc200777e4035812ff9fe9a65e23cd82a52d
Joseph Szczesniak <nukapunk@gmail.com>
2015-05-11 13:52:30 -0500

    Added missing db connection

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var todos = require('./routes/todos');
 
@@ -24,6 +23,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/todos', todos);
+
+// Mongo setup
+var mongoose = require('mongoose');
+
+var mongoURI = "mongodb://localhost:27017/todoAPI";
+var MongoDB = mongoose.connect(mongoURI).connection;
+MongoDB.on('error', function (err) {
+  if (err) {
+    console.log('mongodb connection error', err);
+  } else {
+    console.log('mongodb connection successful');
+  }
+});
+
+MongoDB.once('open', function () {
+  console.log('mongodb connection open');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
